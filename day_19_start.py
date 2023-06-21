b1358d848ffaa1e55941a721e63ac9ef440d95dc
Megan <111366835+MeganKennerley@users.noreply.github.com>
2023-06-21 14:37:34 +0100

    Update day_19_start.py

--- day_19_start.py
+++ day_19_start.py
@@ -1,30 +1,3 @@
-""" Etchasketch """
-# def f():
-#     tim.forward(10)
-#
-# def b():
-#     tim.backward(10)
-#
-# def l():
-#     tim.left(10)
-#
-# def r():
-#     tim.right(10)
-#
-# def c():
-#     tim.clear()
-#     tim.penup()
-#     tim.home()
-#     tim.pendown()
-#
-# screen.listen()
-# screen.onkey(f, "w")
-# screen.onkey(b, "s")
-# screen.onkey(l, "a")
-# screen.onkey(r, "d")
-# screen.onkey(c, "c")
-
-
 from turtle import Turtle, Screen
 import random
 
@@ -39,27 +12,27 @@ all_turtles = []
 is_on = False
 
 for turtle in range(0, 6):
-    michael_the_turtle = Turtle(shape="turtle")
-    michael_the_turtle.penup()
-    michael_the_turtle.color(colors[turtle])
-    michael_the_turtle.goto(x=-230, y=y_positions[turtle])
-    all_turtles.append(michael_the_turtle)
+    turtle = Turtle(shape="turtle")
+    turtle.penup()
+    turtle.color(colors[turtle])
+    turtle.goto(x=-230, y=y_positions[turtle])
+    all_turtles.append(turtle)
 
 if user_bet:
     is_on = True
 
 while is_on:
 
-    for michael in all_turtles:
-        if michael.xcor() > (230):
+    for turtle in all_turtles:
+        if turtle.xcor() > (230):
             is_on = False
-            winning_color = michael.pencolor()
+            winning_color = turtle.pencolor()
             if winning_color.lower() == user_bet.lower():
                 print(f"You win! {winning_color.capitalize()} was the winner!")
             else:
                 print(f"Sorry! You lost! {winning_color.capitalize()} was the winner!")
         random_distance = random.randint(0, 10)
-        michael.forward(random_distance)
+        turtle.forward(random_distance)
 
 
 
